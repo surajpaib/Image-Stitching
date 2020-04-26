de3ddaff4ed504dc77db78c15cfc5a772002fba4
Suraj Pai <surajballambat@gmail.com>
2020-04-26 05:40:23 +0200

    Added harris corner detector

--- keypoint_detector.py
+++ keypoint_detector.py
@@ -3,10 +3,13 @@ import numpy as np
 
 
 class KeypointDetector:
-    def __init__(self):
-        pass
+    def __init__(self, block_size=2):
+        self.block_size = block_size
 
     def set_image(self, image):
         self.image = image
+        self.grayscale = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY).astype(np.float32)
 
     def get_keypoints(self):
+        self.keypoints = cv2.cornerHarris(self.grayscale, self.block_size, 3, 0.004)
+
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import cv2
 import numpy as np
 
-
+from keypoint_detector import KeypointDetector
 
 def main(args):
     first_image = cv2.imread(args.first_image_path)
     second_image = cv2.imread(args.second_image_path)
 
+    kd = KeypointDetector(block_size=args.harris_neighbourhood_size)
+    kd.set_image(first_image)
+    kd.get_keypoints()
+
+
+    pixel_locs = np.argwhere(kd.keypoints>0.01*kd.keypoints.max())
+
+    for pixel in pixel_locs:
+        cv2.imshow('corner_neigbourhood', first_image[pixel[0]-5: pixel[0]+5, pixel[1] - 5: pixel[1]+5])
+        cv2.waitKey(0)
+    print(pixel_locs)
+    cv2.imshow('keypoints', first_image)
+    cv2.waitKey(0)
     
 
 
@@ -16,4 +29,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("first_image_path", help="Path to first image in the pair")
     parser.add_argument("second_image_path", help="Path to second image in the pair")
-    parser.add_argument("--harris_neighbourhood_size", help="Number of pixels in the harris neighbourhood", type=int, default=2)
+    parser.add_argument("--harris_neighbourhood_size", help="Number of pixels in the harris neighbourhood", type=int, default=5)
+    args = parser.parse_args()
+    main(args)
